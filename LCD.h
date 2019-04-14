ca176c416ad526e89d358cb3685285cb1429f2a8
MohamedAliRashad <m.rashadmow@gmial.com>
2019-04-14 12:13:02 +0200

    Documenting

--- LCD.h
+++ LCD.h
@@ -1,16 +1,49 @@
 #ifndef LCD_H
 #define LCD_H
 
-#include "TM4C123.h"
 #include "PORT.h"
+#include "DIO.h"
 
 void LCD_init8bit(void);
 void LCD_init4bit(void);
+/*
+Description: Init the LCD based on what configuration it is working on.
+
+Paramaters:
+    NOTHING
+
+Return:
+    NOTHING
+
+*/
 
 void LCD_Cmd8bit(char command);
 void LCD_Cmd4bit(char command);
+/*
+Description: Send Commands after configuring the LCD to accept it.
 
-void LCD_Send8bit(char x[]);
-void LCD_Send4bit(char x[]);
+Paramaters:
+
+    command: 1 Byte command either sent one time in 8-bit Mode or two times in 4-bit Mode.
+
+Return:
+    NOTHING
+
+*/
+
+void LCD_Send8bit(char x[], int size);
+void LCD_Send4bit(char x[], int size);
+/*
+Description: Send Data after Enabling the LCD to accept it.
+
+Paramaters:
+
+    1- x[]: Array of chars to show on the LCD. 
+    2- size: The size of the array to loop on.
+
+Return:
+    NOTHING
+    
+*/
 
 #endif /* TM4C123_H */
