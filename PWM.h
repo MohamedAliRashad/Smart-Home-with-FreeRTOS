f35af116ec77d662968518af3c8b5d530278f78b
MohamedAliRashad <m.rashadmow@gmial.com>
2019-04-14 12:14:10 +0200

    Basic Headers

---
+++ PWM.h
@@ -0,0 +1,8 @@
+#ifndef PWM_H   /* Include guard */
+#define PWM_H
+
+// include Registers Header
+#include "tm4c123gh6pm.h"
+
+
+#endif // PWM_H
---
+++ UART.h
@@ -0,0 +1,8 @@
+#ifndef UART_H   /* Include guard */
+#define UART_H
+
+// include Registers Header
+#include "tm4c123gh6pm.h"
+
+
+#endif // UART_H
