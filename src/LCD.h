b4863279827efa6fd905a6ef15be1cc348f2122a
MohamedRabie97 <36548647+MohamedRabie97@users.noreply.github.com>
2019-04-23 15:05:05 +0200

    LCD.h file

--- src/LCD.h
+++ src/LCD.h
@@ -1,49 +1,11 @@
-#ifndef LCD_H   /* Include guard */
-#define LCD_H
-
-#include "libraries/DIO.h"
-#include "libraries/PORT.h"
-
-void LCD_init8bit(void);
-void LCD_init4bit(void);
-/*
-Description: Init the LCD based on what configuration it is working on.
-
-Paramaters:
-    NOTHING
-
-Return:
-    NOTHING
-
-*/
-
-void LCD_Cmd8bit(char command);
-void LCD_Cmd4bit(char command);
-/*
-Description: Send Commands after configuring the LCD to accept it.
-
-Paramaters:
-
-    command: 1 Byte command either sent one time in 8-bit Mode or two times in 4-bit Mode.
-
-Return:
-    NOTHING
-
-*/
-
-void LCD_Send8bit(char x[], int size);
-void LCD_Send4bit(char x[], int size);
-/*
-Description: Send Data after Enabling the LCD to accept it.
-
-Paramaters:
-
-    1- x[]: Array of chars to show on the LCD. 
-    2- size: The size of the array to loop on.
-
-Return:
-    NOTHING
-    
-*/
-
-#endif /* LCD_H */
+#ifndef LCD_h
+#define LCD_h
+#include "stdint.h"
+/*LCD DRIVERS*/
+void systick_wait (uint32_t delay);
+void delayMS(uint32_t n);
+void delayUS(uint32_t N);
+void LCD_data(char data);
+void LCD_command(char com);
+void LCD_init();
+#endif
