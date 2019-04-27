f31e802367fb0b1a3060a9c58042c624281db0df
MohamedAliRashad <m.rashadnow@gmail.com>
2019-04-27 13:29:49 +0200

    Delay Header

---
+++ libraries/delay.h
@@ -0,0 +1,13 @@
+#ifndef DELAY_H /* Include guard */
+#define DELAY_H
+
+#include "stdint.h"
+#include "tm4c123gh6pm.h"
+
+void systick_wait(uint32_t delay);
+
+void delayMS(uint32_t n);
+
+void delayUS(uint32_t N);
+
+#endif /* DELAY_H */
