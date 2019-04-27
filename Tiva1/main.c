b2c7e8d031d6c88314b6ccdc46d1a7d4c9117d2c
MohamedAliRashad <m.rashadnow@gmail.com>
2019-04-27 13:34:06 +0200

    Adding FreeRTOS Blinking Example

--- Tiva1/main.c
+++ Tiva1/main.c
@@ -1,16 +1,47 @@
-#include "../src/Motor.h"
-#include "../libraries/DIO.h"
-#include "../libraries/PORT.h"
+#include <stdint.h>
+#include <FreeRTOS.h>
+#include <task.h>
 
-void main(void){
+void blink_init(void);
 
-    // TODO:
-
-
+void vBlinkTask(void *pvParameters);
 
 
+int main(void)
+{
+    //Blink Task runs here
+    xTaskCreate(vBlinkTask, "BlinkTask", 1000, NULL, 1, NULL);
 
+    vTaskStartScheduler();
+    while(1); //should never reach here
+}
 
+void vBlinkTask(void *pvParameters)
+{
+    blink_init();
+    //infinite loop
+    while(1)
+    {
+        GPIOF->DATA = 0x04;
+        //delay(1000);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        GPIOF->DATA = 0x08;
+        //delay(1000);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        
+    }
 
-    return;
+}
+
+void blink_init(void)
+{
+    //This is just initialization
+    SYSCTL->RCGCGPIO |= 0x20;
+	GPIOF->DIR = 0x0e;
+	GPIOF->DEN = 0x0e;
+
+	GPIOF->DATA = 0x02;
+    //delay(3000);
+    vTaskDelay(pdMS_TO_TICKS(3000));
+    //GPIOF->DATA = 0x02;
 }
--- libraries/PWM.h
+++ libraries/PWM.h
@@ -1,8 +1,39 @@
-#ifndef PWM_H   /* Include guard */
+#ifndef PWM_H /* Include guard */
 #define PWM_H
 
 // include Registers Header
 #include "tm4c123gh6pm.h"
+#include <stdint.h>
 
+typedef unsigned char bool;
+
+void InitTimers();
+/*
+Description: Initialize the PWM Timers by
+    1- Enable the PWM clock through the RCGC0.
+    2- Enable the clock to the appropriate GPIO module via RCGC2   <check with the others if no one accessed this>
+
+Parameters:
+    Nothing
+
+Return:
+    Nothing
+*/
+
+bool SetPinFrequency(int8_t pin, uint32_t frequency);
+
+void pwmWrite(uint8_t pin, uint8_t val);
+/*
+Description: 
+    1- Enable the PWM clock through the RCGC0.
+    2- Enable the clock to the appropriate GPIO module via RCGC2   <check with the others if no one accessed this>
+
+Parameters:
+    1- pin: Pin Number of the port.
+    2- val: 
+
+Return:
+    Nothing
+*/
 
 #endif /* PWM_H */
