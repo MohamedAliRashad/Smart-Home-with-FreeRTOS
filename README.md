d76b75ae5ef896e40419e06f6a15a9ae9d923932
MohamedAliRashad <m.rashadnow@gmail.com>
2019-05-03 22:01:31 +0200

    My vision for a PWM Header

---
+++ README.md
@@ -0,0 +1,6 @@
+# Smart-Home
+Smart Home project based on Tiva C
+
+# Project Description
+
+![alt text](https://github.com/MohamedAliRashad/Smart-Home/blob/master/images/Project_Description.png)
--- libraries/PWM.h
+++ libraries/PWM.h
@@ -5,8 +5,6 @@
 #include "tm4c123gh6pm.h"
 #include <stdint.h>
 
-typedef unsigned char bool;
-
 void InitTimers();
 /*
 Description: Initialize the PWM Timers by
@@ -20,17 +18,33 @@ Return:
     Nothing
 */
 
-bool SetPinFrequency(int8_t pin, uint32_t frequency);
-
-void pwmWrite(uint8_t pin, uint8_t val);
+bool SetPinFrequency(int8_t pin, uint32_t freq);
 /*
-Description: 
-    1- Enable the PWM clock through the RCGC0.
-    2- Enable the clock to the appropriate GPIO module via RCGC2   <check with the others if no one accessed this>
+Description: Sets the frequency of the PWM signal for a pin.
 
 Parameters:
     1- pin: Pin Number of the port.
-    2- val: 
+    2- freq: The prescaller value for the tiva frequency.
+        Example:
+            Set pin 6's PWM frequency to 62500 Hz (62500/1 = 62500)
+            The base frequency here was 62500 Hz
+            setPwmFrequency(6, 1);
+        Another Example:
+            Set pin 9's PWM frequency to 3906 Hz (31250/8 = 3906)
+            The base frequency here was 3906 Hz
+            setPwmFrequency(9, 8);
+
+Return:
+    Nothing
+*/
+
+void pwmWrite(uint8_t pin, uint8_t val);
+/*
+Description: Writes an analog value (PWM wave) to a pin.
+
+Parameters:
+    1- pin: Pin Number of the port.
+    2- val: The duty cycle between 0 (always off) - 255 (always on) 
 
 Return:
     Nothing
