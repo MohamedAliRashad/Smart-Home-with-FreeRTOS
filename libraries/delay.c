06e1f95c6135a98bbbf8cb6b3c8c25185bf9b163
MohamedRabie97 <36548647+MohamedRabie97@users.noreply.github.com>
2019-05-11 16:31:03 +0200

    Delay

--- libraries/delay.c
+++ libraries/delay.c
@@ -1,7 +1,15 @@
 #include "delay.h"
 
+void systick_init(void){
+	NVIC_ST_CTRL_R = 0;
+	NVIC_ST_RELOAD_R = 0x00FFFFFF;
+	NVIC_ST_CURRENT_R = 0;
+	NVIC_ST_CTRL_R = 0x00000005;
+
+}
 void systick_wait(uint32_t delay)
 {
+	systick_init();
 	NVIC_ST_RELOAD_R = delay - 1;
 	NVIC_ST_CURRENT_R = 0;
 	while ((NVIC_ST_CTRL_R & 0x0001000) == 0)
@@ -25,3 +33,4 @@ void delayUS(uint32_t N)
 		systick_wait(16);
 	}
 }
+
