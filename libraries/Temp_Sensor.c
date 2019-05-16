4a941a615108ebe79f2e53a739caebdb8e1feeef
MohamedAliRashad <m.rashadnow@gmail.com>
2019-05-16 06:15:14 +0200

    Adding Temperature Sensor

--- libraries/Temp_Sensor.c
+++ libraries/Temp_Sensor.c
@@ -1,20 +1,19 @@
 #include "Temp_Sensor.h"
 
 
-void TempSensor_init()
+void TempSensor_init(void)
 {
 
-    ADC0_init(AIN0, TIMER_TRIGGER); /* Initialize ADC with Analog Input Channel 0 and timer periodic triggering */
+  ADC0_init(AIN0, TIMER_TRIGGER); /* Initialize ADC with Analog Input Channel 0 and timer periodic triggering */
 	ADC0_SSCTL3_R |= 0x0E;          /* set register content to 0000 1110 */
                                     /* reset bit0: single ended input */	
                                     /* Set bit3: for internal temp sensor enabling */	
 }
 
-/*
-float TempSensor_readTemp()
+float TempSensor_readTemp(void)
 {
-
- 
+    float temp = 0.0;
+    ADC0_start_conversion(); 
+		temp = 147.5 - (247 * result) / 4096; /* from the datasheet */
+    return temp;
 }
-
-*/
--- libraries/Temp_Sensor.h
+++ libraries/Temp_Sensor.h
@@ -4,19 +4,18 @@
 * 30/4/2019
 *
 */
+#ifndef TEMP_H
+#define TEMP_H
 
 #include "stdint.h"
 #include "tm4c123gh6pm.h"
 #include "ADC.h"
 
-
-void TempSensor_init();
+void TempSensor_init(void);
         /* Initialize Temperature sensor */
 
 
-float TempSensor_readTemp();
+float TempSensor_readTemp(void);
         /* read Temperature from sensor */
         
-
- 
-
+#endif
